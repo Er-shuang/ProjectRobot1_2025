31a2f57469891f1013158092e7e388456def94ce
Er-shuang <1343109389@qq.com>
2025-06-21 17:30:34 +0800

    Display number result in terminal

--- display_numbers.py
+++ display_numbers.py
@@ -7,25 +7,14 @@ from microbit import *
 
 # Define 1,2,3 numbers
 numbers = {
-    "1": Image("00100:"
-               "01100:"
-               "00100:"
-               "00100:"
-               "01110"),
-    "2": Image("00110:"
-               "01001:"
-               "00010:"
-               "00100:"
-               "01111"),
-    "3": Image("01100:"
-               "00010:"
-               "00100:"
-               "00010:"
-               "01100")
+    "1": Image("00100:" "01100:" "00100:" "00100:" "01110"),
+    "2": Image("00110:" "01001:" "00010:" "00100:" "01111"),
+    "3": Image("01100:" "00010:" "00100:" "00010:" "01100"),
 }
 
 # Display 1,2,3 numbers in a circle
 while True:
     for number in ["1", "2", "3"]:
         display.show(numbers[number])  # Display numbers
+        print("Now displaying:", num)  # Display result in terminal
         sleep(1000)  # Each number display for 1 second
