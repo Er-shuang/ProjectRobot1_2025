05b8a71942ac9a018e5ed6bd8247ccfeeac52dd5
Er-shuang <1343109389@qq.com>
2025-06-21 18:52:24 +0800

    Changed the order and brightness of the displayed numbers

--- display_numbers.py
+++ display_numbers.py
@@ -7,14 +7,14 @@ from microbit import *
 
 # Define 1,2,3 numbers
 numbers = {
-    "1": Image("00100:" "01100:" "00100:" "00100:" "01110"),
-    "2": Image("00110:" "01001:" "00010:" "00100:" "01111"),
-    "3": Image("01100:" "00010:" "00100:" "00010:" "01100"),
+    "1": Image("09990:" "00900:" "00900:" "00990:" "00900"),
+    "2": Image("99990:" "00900:" "09000:" "90090:" "09900"),
+    "3": Image("09900:" "90090:" "09000:" "90000:" "09900"),
 }
 
 # Display 1,2,3 numbers in a circle
 while True:
     for number in ["1", "2", "3"]:
         display.show(numbers[number])  # Display numbers
-        print("Now displaying:", number)  # Display result in terminal
+        print("Now displaying: ", number)  # Display result in terminal
         sleep(1000)  # Each number display for 1 second
